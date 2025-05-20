b2aebd9b19f768a09f5a18d10ecb53cfbfd8d062
Miguel Angel <miguelangeldev@icloud.com>
2025-05-20 19:58:44 -0400

    project init

---
+++ app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+
+    <div class="hello-world">
+      Hello World
+    </div>
+  </div>
+</template>
+
+<style>
+.hello-world {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100dvh;
+  font-size: 5rem;
+  font-weight: bold;
+}
+</style>
